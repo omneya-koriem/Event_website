de83a7ea03195caffa89e7d5d1f727c2dacd532e
Omneya Koriem <omneyakoriem@yahoo.com>
2019-05-29 12:21:07 +0200

    first commit

---
+++ project_file/gulpfile.js
@@ -0,0 +1,32 @@
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+ 
+sass.compiler = require('node-sass');
+ 
+gulp.task('scss', function () {
+  return gulp.src('./scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+ 
+gulp.task('watch', function () {
+  gulp.watch('./scss/*.scss', gulp.series('scss'));
+});
+var gulp = require('gulp'),
+    watch = require('gulp-watch');
+ 
+gulp.task('stream', function () {
+    // Endless stream mode
+    return watch('css/**/*.css', { ignoreInitial: false })
+        .pipe(gulp.dest('build'));
+});
+ 
+gulp.task('callback', function () {
+    // Callback mode, useful if any plugin in the pipeline depends on the `end`/`flush` event
+    return watch('css/**/*.css', function () {
+        gulp.src('css/**/*.css')
+            .pipe(gulp.dest('build'));
+    });
+});
